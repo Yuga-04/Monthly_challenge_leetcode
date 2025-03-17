fecc5b92d30099043774e3b98dc0bd19586ac0ff
Yuga T <ec1yugat@gmail.com>
2025-03-17 15:15:52 +0530

    Create DivideArrayIntoEqualPairs.java

---
+++ March_2025/Day_17/DivideArrayIntoEqualPairs.java
@@ -0,0 +1,13 @@
+//question : https://leetcode.com/problems/divide-array-into-equal-pairs/description/
+
+class Solution {
+    public boolean divideArray(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i += 2) {
+            if (nums[i] != nums[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
