a814e3e7a3d58ba60607537001437c056042a2ea
Yuga T <ec1yugat@gmail.com>
2025-03-18 17:19:40 +0530

    Create LongestNiceSubarray.java

---
+++ March_2025/Day_18/LongestNiceSubarray.java
@@ -0,0 +1,22 @@
+//Question : https://leetcode.com/problems/longest-nice-subarray/description/
+class Solution {
+    public int longestNiceSubarray(int[] nums) {
+        int maxLength = 0;
+        int orValue = 0;
+        int left = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            
+            while ((orValue & nums[right]) != 0) {
+                orValue ^= nums[left];
+                left++;
+            }
+
+            orValue |= nums[right];
+            
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
