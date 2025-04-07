d54aceb78727d3ce92a008f1bde6e181af89353b
Yuga T <ec1yugat@gmail.com>
2025-04-07 13:45:24 +0530

    Create PartitionEqualSubsetSum.java

---
+++ April_2025/Day_07/PartitionEqualSubsetSum.java
@@ -0,0 +1,21 @@
+//Question : https://leetcode.com/problems/partition-equal-subset-sum/description/
+
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int total = 0;
+        for (int num : nums) total += num;
+        if (total % 2 != 0) return false;
+
+        int target = total / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+
+        for (int num : nums) {
+            for (int i = target; i >= num; i--) {
+                dp[i] = dp[i] || dp[i - num];
+            }
+        }
+
+        return dp[target];
+    }
+}
