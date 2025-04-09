394b1aff4673be04fc47911611d9454bf0a24a23
Yuga T <ec1yugat@gmail.com>
2025-04-09 19:05:20 +0530

    Create  CheckifNumberisaSumofPowersofThree.java

---
+++ March_2025/Day_04/CheckifNumberisaSumofPowersofThree.java
@@ -0,0 +1,13 @@
+//Question : https://leetcode.com/problems/check-if-number-is-a-sum-of-powers-of-three/description/
+
+class Solution {
+    public boolean checkPowersOfThree(int n) {
+        while (n > 0) {
+            if (n % 3 == 2) {
+                return false;
+            }
+            n /= 3;
+        }
+        return true;
+    }
+}
