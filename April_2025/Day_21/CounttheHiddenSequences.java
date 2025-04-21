faf9f4a770bbc84e3cac5252de2e9d002ac344af
Yuga T <201981477+Yuga-04@users.noreply.github.com>
2025-04-21 20:13:37 +0530

    Create CounttheHiddenSequences.java

---
+++ April_2025/Day_21/CounttheHiddenSequences.java
@@ -0,0 +1,18 @@
+//Question : https://leetcode.com/problems/count-the-hidden-sequences/description/
+
+class Solution {
+    public int numberOfArrays(int[] differences, int lower, int upper) {
+        long minPrefix = 0, maxPrefix = 0, sum = 0;
+
+        for (int diff : differences) {
+            sum += diff;
+            minPrefix = Math.min(minPrefix, sum);
+            maxPrefix = Math.max(maxPrefix, sum);
+        }
+
+        long minStart = lower - minPrefix;
+        long maxStart = upper - maxPrefix;
+
+        return (int)Math.max(0, maxStart - minStart + 1);
+    }
+}
