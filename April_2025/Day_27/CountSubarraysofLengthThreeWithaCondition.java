020707a507c1547ec58c578391e0b157add9ab8a
Yuga T <201981477+Yuga-04@users.noreply.github.com>
2025-04-27 12:46:53 +0530

    Create CountSubarraysofLengthThreeWithaCondition.java

---
+++ April_2025/Day_27/CountSubarraysofLengthThreeWithaCondition.java
@@ -0,0 +1,11 @@
+//Question : https://leetcode.com/problems/count-subarrays-of-length-three-with-a-condition/description/
+
+class Solution {
+    public int countSubarrays(int[] nums) {
+        int count=0,n=nums.length;
+        for(int i=0;i<n-2;i++){
+            if((nums[i]+nums[i+2])*2==nums[i+1]) count++;
+        }
+        return count;
+    }
+}
