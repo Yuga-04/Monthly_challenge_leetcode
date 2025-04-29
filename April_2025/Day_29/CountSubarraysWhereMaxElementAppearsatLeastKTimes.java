ecff7c5c9d2d4dd541dd061b3a7455eb44d4bf35
Yuga T <201981477+Yuga-04@users.noreply.github.com>
2025-04-29 11:07:13 +0530

    Create CountSubarraysWhereMaxElementAppearsatLeastKTimes.java

---
+++ April_2025/Day_29/CountSubarraysWhereMaxElementAppearsatLeastKTimes.java
@@ -0,0 +1,20 @@
+//Auestion : http://leetcode.com/problems/count-subarrays-where-max-element-appears-at-least-k-times/description/
+
+class Solution {
+    public long countSubarrays(int[] nums, int k) {
+        int maxElement = Arrays.stream(nums).max().getAsInt();
+        long res = 0;
+        int left = 0, maxiCount = 0;
+        
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == maxElement) maxiCount++;
+            while (maxiCount == k) {
+                res += nums.length - right;
+                if (nums[left] == maxElement) maxiCount--;
+                left++;
+            }
+        }
+        
+        return res;
+    }
+}
