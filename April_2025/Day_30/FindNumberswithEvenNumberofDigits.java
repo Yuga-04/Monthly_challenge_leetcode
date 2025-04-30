c401952f1ae5be2b4709169836919ee20a13d771
Yuga T <201981477+Yuga-04@users.noreply.github.com>
2025-04-30 12:16:48 +0530

    Create FindNumberswithEvenNumberofDigits.java

---
+++ April_2025/Day_30/FindNumberswithEvenNumberofDigits.java
@@ -0,0 +1,14 @@
+//Question : https://leetcode.com/problems/find-numbers-with-even-number-of-digits/description/
+class Solution {
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        for (int x : nums) {
+            int digits = 0;
+            for (; x > 0; x /= 10)
+                digits++;
+            if ((digits & 1) == 0)
+                count++;
+        }
+        return count;
+    }
+}
